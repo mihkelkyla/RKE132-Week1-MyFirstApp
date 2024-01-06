50d83215e320ce7b310cc84e2af6d897232a8b0d
Mihkel Küla <mihkel.kula@tktk.ee>
2024-01-06 16:52:54 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// This is a comment to my code
+
+
+
+Console.WriteLine("Enter your name:");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" +  userName);
+
+Console.WriteLine($"Hello, {userName} !");
